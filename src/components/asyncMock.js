7c684828e383fbea3b0f76f48c0f1ce530ed413b
edmundopiel <147783117+edmundopiel@users.noreply.github.com>
2024-02-22 20:08:31 -0300

    Segunda Entrega

---
+++ src/components/asyncMock.js
@@ -0,0 +1,39 @@
+const shoeProducts = [
+  { 
+    id: 1, 
+    name: 'Zapatillas Deportivas A', 
+    brand: 'Nike', 
+    price: 120, 
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_746129-MLA70303935102_072023-F.webp' 
+  },
+  { 
+    id: 2, 
+    name: 'Zapatillas Casuales B', 
+    brand: 'Adidas', 
+    price: 90, 
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_750707-MLA42394012708_062020-F.webp' 
+  },
+  { 
+    id: 48, 
+    name: 'Zapatillas de Moda X', 
+    brand: 'Puma', 
+    price: 80, 
+    img: 'https://briganti.com.ar/cdn/shop/products/MCZP05290081-01_1000x.jpg?v=1615301420' 
+  },
+  { 
+    id: 49, 
+    name: 'Zapatillas Deportivas Y', 
+    brand: 'Reebok', 
+    price: 110, 
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_746129-MLA70303935102_072023-F.webp' 
+  },
+  { 
+    id: 50, 
+    name: 'Zapatillas Running Z', 
+    brand: 'Under Armour', 
+    price: 95, 
+    img: 'https://http2.mlstatic.com/D_NQ_NP_2X_606926-MLA45393077362_032021-F.webp' 
+  },
+];
+
+export default shoeProducts;
