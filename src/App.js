c4a92d148e2b28515ccef2d2ddb14473d33e7447
edmundopiel <147783117+edmundopiel@users.noreply.github.com>
2024-02-22 20:07:01 -0300

    Segunda Entrega

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer';
-
+import ItemCount from './components/ItemCount';
 
 const App = () => {
+  const handleAddToCart = (quantity) => {
+    console.log(`Adding ${quantity} items to the cart`);
+    // Puedes realizar acciones adicionales aquí, como agregar productos al carrito
+  };
+
   return (
     <div className="app-container">
       <NavBar />
       <ItemListContainer greeting="¡Bienvenido a nuestra tienda!" />
+      <ItemCount stock={10} initial={1} onAdd={handleAddToCart} />
     </div>
   );
 };
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import shoeProducts from './asyncMock';
+import ItemCount from './ItemCount';
+import '../styles.css';
 
-const ItemListContainer = ({ greeting }) => {
+const Item = ({ product }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  const handleAddToCart = () => {
+    console.log(`Adding ${quantity} ${product.name} to the cart`);
+
+  };
+
+  return (
+    <li key={product.id}>
+      <img
+        src={product.img}
+        alt={product.name}
+        style={{ width: '100px', height: '100px', objectFit: 'cover' }}
+      />
+      <strong>{product.name}</strong> - {product.brand} - ${product.price}
+      <ItemCount stock={10} initial={1} onAdd={(qty) => setQuantity(qty)} />
+      <button onClick={handleAddToCart}>Add to Cart</button>
+    </li>
+  );
+};
+
+const ItemListContainer = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = () => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(shoeProducts);
+        }, 1000); // Simulamos una carga asíncrona de 1 segundo
+      });
+    };
+
+    fetchProducts().then((data) => setProducts(data));
+  }, []);
+
   return (
     <div>
-      <p>{greeting}</p>
+      <h2>Catalogo de Zapatillas</h2>
+      <ul>
+        {products.map((product) => (
+          <Item key={product.id} product={product} />
+        ))}
+      </ul>
     </div>
   );
 };
